dc73d0c8d2f4f30f548de47656ca4e7d45471a48
AiueoKashi <akkey2017@gmail.com>
2021-02-05 02:11:35 +0000

    Fix github actions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server =  http.createServer(function(request, response) {
 		response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     response.end('<h1>DUCK</h1>');
 	})
-  server.listen(8080);
+  server.listen(3000);
 
 const { Client, Collection } = require('discord.js');
 const client = new Client({
@@ -21,6 +21,7 @@ client.evalLog = "803971607504617493";
 client.spamLog = "803969724651405322";
 client.commandLog = "803969058725822486";
 client.guildId = "803967839458689074";
+
 
 require('./eventLoader/loadEvents.js')(client);
 
